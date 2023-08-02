2090554efdc35896eaba9ed1bd00f47f4cfa5e8f
Kazuki Kijima <kazukiyoshida96@gmail.com>
2023-08-01 19:56:45 -0700

    :art: Slow down the refresh rate to avoid `Too many requests` error

--- tusk/timeline.py
+++ tusk/timeline.py
@@ -2,8 +2,10 @@ import time
 import html2text
 import requests
 
+from .constants import REFRESH_RATE
 
-def timeline(instance_url: str, access_token: str):
+
+def timeline(instance_url: str, access_token: str) -> None:
     since_id = None
     request_url = f"https://{instance_url}/api/v1/timelines/home"
     while True:
@@ -12,14 +14,25 @@ def timeline(instance_url: str, access_token: str):
             headers={"Authorization": f"Bearer {access_token}"},
             timeout=60,
         )
-        for post in reversed(response.json()):
-            print(post["id"])
-            print(post["account"]["display_name"])
-            if spoiler_text := post["spoiler_text"]:
-                print(f"[spoiler_text]: {spoiler_text}")
-            h = html2text.HTML2Text()
-            print(h.handle(post["content"]), end="")
-            since_id = post["id"]
 
-        # TODO: Define a constant for the refresh rate
-        time.sleep(1)
+        if response.status_code == 429:
+            print(f"{response.status_code=}")
+            print(f"{response.json()}=")  # {'error': 'Too many requests'}
+
+        elif response.status_code == 200:
+            for post in reversed(response.json()):
+                print(post["id"])
+                print(post["account"]["display_name"])
+                if spoiler_text := post["spoiler_text"]:
+                    print(f"[spoiler_text]: {spoiler_text}")
+                h = html2text.HTML2Text()
+                print(h.handle(post["content"]), end="")
+                since_id = post["id"]
+
+            time.sleep(REFRESH_RATE)
+
+        else:
+            print(f"{response.status_code=}")
+            print(f"{response.text=}")
+            print("Unknown error occurred. Quitting timeline view.")
+            return
